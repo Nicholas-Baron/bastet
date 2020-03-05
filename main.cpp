4401f282778fcdcb94be34263d5ef76cf78f310f
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-05 10:50:19 -0800

    Removed tabs

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 /*
-	Bastet - tetris clone with embedded bastard block chooser
-	(c) 2005-2009 Federico Poloni <f.polonithirtyseven@sns.it> minus 37
+    Bastet - tetris clone with embedded bastard block chooser
+    (c) 2005-2009 Federico Poloni <f.polonithirtyseven@sns.it> minus 37
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <boost/assign.hpp>
@@ -32,36 +32,36 @@ using namespace boost;
 using namespace boost::assign;
 
 int main() {
-	Ui ui;
-	while (1) {
-		int choice = ui.MenuDialog(
-			list_of("Play! (normal version)")("Play! (harder version)")(
-				"View highscores")("Customize keys")("Quit"));
-		switch (choice) {
-			case 0: {
-				// ui.ChooseLevel();
-				BastetBlockChooser bc;
-				ui.Play(&bc);
-				ui.HandleHighScores(difficulty_normal);
-				ui.ShowHighScores(difficulty_normal);
-			} break;
-			case 1: {
-				// ui.ChooseLevel();
-				NoPreviewBlockChooser bc;
-				ui.Play(&bc);
-				ui.HandleHighScores(difficulty_hard);
-				ui.ShowHighScores(difficulty_hard);
-			} break;
-			case 2:
-				ui.ShowHighScores(difficulty_normal);
-				ui.ShowHighScores(difficulty_hard);
-				break;
-			case 3:
-				ui.CustomizeKeys();
-				break;
-			case 4:
-				exit(0);
-				break;
-		}
-	}
+    Ui ui;
+    while (1) {
+        int choice = ui.MenuDialog(
+            list_of("Play! (normal version)")("Play! (harder version)")(
+                "View highscores")("Customize keys")("Quit"));
+        switch (choice) {
+            case 0: {
+                // ui.ChooseLevel();
+                BastetBlockChooser bc;
+                ui.Play(&bc);
+                ui.HandleHighScores(difficulty_normal);
+                ui.ShowHighScores(difficulty_normal);
+            } break;
+            case 1: {
+                // ui.ChooseLevel();
+                NoPreviewBlockChooser bc;
+                ui.Play(&bc);
+                ui.HandleHighScores(difficulty_hard);
+                ui.ShowHighScores(difficulty_hard);
+            } break;
+            case 2:
+                ui.ShowHighScores(difficulty_normal);
+                ui.ShowHighScores(difficulty_hard);
+                break;
+            case 3:
+                ui.CustomizeKeys();
+                break;
+            case 4:
+                exit(0);
+                break;
+        }
+    }
 }
